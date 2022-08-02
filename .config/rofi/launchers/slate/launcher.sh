82eca67c10acd7e99c76393873a1ed30ed51c557
Trent Deal <trent.deal@gmail.com>
2022-08-02 15:46:00 -0400

    Setup launchers how I like em

--- .config/rofi/launchers/slate/launcher.sh
+++ .config/rofi/launchers/slate/launcher.sh
@@ -1,27 +1,9 @@
-#!/usr/bin/env bash
-
-## Author  : Aditya Shakya
-## Mail    : adi1090x@gmail.com
-## Github  : @adi1090x
-## Twitter : @adi1090x
-
-# Available Styles
-# >> Created and tested on : rofi 1.6.0-1
-#
-# slate_full     slate_center     slate_left
-# slate_right    slate_top        slate_bottom
-
-theme="slate_full"
-
 dir="$HOME/.config/rofi/launchers/slate"
-styles=($(ls -p --hide="colors.rasi" $dir/styles))
-color="${styles[$(( $RANDOM % 20 ))]}"
+theme="$1"
 
-# comment this line to disable random colors
-sed -i -e "s/@import .*/@import \"$color\"/g" $dir/styles/colors.rasi
+if [ ! -f "$dir/themes/$theme.rasi" ]; then
+    echo "Theme '$theme' does not exist!"
+    exit 1
+fi
 
-# comment these lines to disable random style
-themes=($(ls -p --hide="launcher.sh" --hide="styles" $dir))
-theme="${themes[$(( $RANDOM % 6 ))]}"
-
-rofi -no-lazy-grab -show drun -modi drun -theme $dir/"$theme"
+rofi -no-lazy-grab -show drun -modi drun -theme "$dir/themes/$theme.rasi"
