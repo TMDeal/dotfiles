6ec807514255e5c12448408f833132860e5efe74
Trent Deal <trent.deal@gmail.com>
2023-01-30 22:23:18 -0500

    Use polybar to discover monitors instead of xrandr

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -12,10 +12,6 @@ if [ ! -d "$LOGDIR" ]; then
     mkdir -p $LOGDIR
 fi
 
-if type "xrandr"; then
-    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        MONITOR=$m polybar --reload top 2>"$LOGDIR/$m.log" &
-    done
-else
-    polybar --reload top 2>"$LOGDIR"/default.log &
-fi
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload top 2>"$LOGDIR/$m.log" &
+done
