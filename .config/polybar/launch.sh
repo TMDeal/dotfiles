d0fdcffb4e456e540e283cd8546efaef3334dadf
Trent Deal <trent.deal@gmail.com>
2023-01-30 21:51:57 -0500

    Update polybar config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,5 +6,16 @@ killall -q polybar
 # wait until fully dead
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# run it again
-polybar --reload top 2>$HOME/.config/polybar/logs &
+LOGDIR="$HOME/.cache/polybar/logs"
+
+if [ ! -d "$LOGDIR" ]; then
+    mkdir -p $LOGDIR
+fi
+
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload top 2>"$LOGDIR/$m.log" &
+    done
+else
+    polybar --reload top 2>"$LOGDIR"/default.log &
+fi
