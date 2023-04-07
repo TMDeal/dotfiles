d6f6d6a73898c8d781dbaba2aa6bc62d13e63a68
Trent Deal <trent.deal@gmail.com>
2023-04-07 08:34:59 -0400

    Update mkdnflow config

--- .config/nvim/lua/user/plugins/configs/mkdnflow.lua
+++ .config/nvim/lua/user/plugins/configs/mkdnflow.lua
@@ -3,7 +3,7 @@ if not mkdnflow_ok then
     return
 end
 
-mkdnflow.setup {
+mkdnflow.setup({
     modules = {
         bib = true,
         buffers = true,
@@ -14,70 +14,137 @@ mkdnflow.setup {
         lists = true,
         maps = true,
         paths = true,
-        tables = true
+        tables = true,
+        yaml = false,
     },
-    filetypes = {md = true, rmd = true, markdown = true, pandoc = true},
-    create_dirs = true,             
+    filetypes = { md = true, rmd = true, markdown = true },
+    create_dirs = true,
     perspective = {
-        priority = 'first',
-        fallback = 'current',
+        priority = "first",
+        fallback = "current",
         root_tell = false,
-        nvim_wd_heel = true
-    },    
-    wrap = true,
+        nvim_wd_heel = false,
+        update = false,
+    },
+    wrap = false,
     bib = {
         default_path = nil,
-        find_in_root = true
+        find_in_root = true,
     },
     silent = false,
     links = {
-        style = 'markdown',
-        conceal = true,
+        style = "markdown",
+        name_is_source = false,
+        conceal = false,
+        context = 0,
         implicit_extension = nil,
-        transform_implicit = "md",
-        transform_explicit = false
+        transform_implicit = false,
+        transform_explicit = function(text)
+            text = text:gsub(" ", "-")
+            text = text:lower()
+            text = os.date("%Y-%m-%d_") .. text
+            return text
+        end,
+    },
+    new_file_template = {
+        use_template = false,
+        placeholders = {
+            before = {
+                title = "link_title",
+                date = "os_date",
+            },
+            after = {},
+        },
+        template = "# {{ title }}",
     },
     to_do = {
-        symbols = {' ', '-', 'X'},
+        symbols = { " ", "-", "X" },
         update_parents = true,
-        not_started = ' ',
-        in_progress = '-',
-        complete = 'X'
+        not_started = " ",
+        in_progress = "-",
+        complete = "X",
     },
     tables = {
         trim_whitespace = true,
-        format_on_move = true
+        format_on_move = true,
+        auto_extend_rows = false,
+        auto_extend_cols = false,
+    },
+    yaml = {
+        bib = { override = false },
     },
     mappings = {
-        MkdnEnter = {{'n', 'v'}, '<CR>'},
+        MkdnEnter = { { "n", "v" }, "<CR>" },
         MkdnTab = false,
         MkdnSTab = false,
-        MkdnNextLink = {'n', '<Tab>'},
-        MkdnPrevLink = {'n', '<S-Tab>'},
-        MkdnNextHeading = {'n', ']]'},
-        MkdnPrevHeading = {'n', '[['},
-        MkdnGoBack = {'n', '<BS>'},
-        MkdnGoForward = {'n', '<Del>'},
-        MkdnFollowLink = false, -- see MkdnEnter
-        MkdnDestroyLink = {'n', '<M-CR>'},
-        MkdnMoveSource = {'n', '<F2>'},
-        MkdnYankAnchorLink = {'n', 'ya'},
-        MkdnYankFileAnchorLink = {'n', 'yfa'},
-        MkdnIncreaseHeading = {'n', '+'},
-        MkdnDecreaseHeading = {'n', '-'},
-        MkdnToggleToDo = {{'n', 'v'}, '<C-Space>'},
+        MkdnNextLink = { "n", "<Tab>" },
+        MkdnPrevLink = { "n", "<S-Tab>" },
+        MkdnNextHeading = { "n", "]]" },
+        MkdnPrevHeading = { "n", "[[" },
+        MkdnGoBack = { "n", "<BS>" },
+        MkdnGoForward = { "n", "<Del>" },
+        MkdnCreateLink = false,
+        MkdnCreateLinkFromClipboard = false,
+        MkdnFollowLink = false,
+        MkdnDestroyLink = { "n", "<M-CR>" },
+        MkdnTagSpan = { "v", "<M-CR>" },
+        MkdnMoveSource = { "n", "<F2>" },
+        MkdnYankAnchorLink = { "n", "yaa" },
+        MkdnYankFileAnchorLink = { "n", "yfa" },
+        MkdnIncreaseHeading = { "n", "+" },
+        MkdnDecreaseHeading = { "n", "-" },
+        MkdnToggleToDo = { { "n", "v" }, "<C-Space>" },
         MkdnNewListItem = false,
+        MkdnNewListItemBelowInsert = { "n", "o" },
+        MkdnNewListItemAboveInsert = { "n", "O" },
         MkdnExtendList = false,
         MkdnUpdateNumbering = false,
-        MkdnTableNextCell = {'i', '<Tab>'},
-        MkdnTablePrevCell = {'i', '<S-Tab>'},
+        MkdnTableNextCell = { "i", "<Tab>" },
+        MkdnTablePrevCell = { "i", "<S-Tab>" },
         MkdnTableNextRow = false,
-        MkdnTablePrevRow = {'i', '<M-CR>'},
+        MkdnTablePrevRow = { "i", "<M-CR>" },
         MkdnTableNewRowBelow = false,
         MkdnTableNewRowAbove = false,
         MkdnTableNewColAfter = false,
         MkdnTableNewColBefore = false,
         MkdnFoldSection = false,
-        MkdnUnfoldSection = false
-    }
-}
+        MkdnUnfoldSection = false,
+
+        -- DEFAULTS
+        -- MkdnEnter = { { "n", "v" }, "<CR>" },
+        -- MkdnTab = false,
+        -- MkdnSTab = false,
+        -- MkdnNextLink = { "n", "<Tab>" },
+        -- MkdnPrevLink = { "n", "<S-Tab>" },
+        -- MkdnNextHeading = { "n", "]]" },
+        -- MkdnPrevHeading = { "n", "[[" },
+        -- MkdnGoBack = { "n", "<BS>" },
+        -- MkdnGoForward = { "n", "<Del>" },
+        -- MkdnCreateLink = false,                                -- see MkdnEnter
+        -- MkdnCreateLinkFromClipboard = { { "n", "v" }, "<leader>p" }, -- see MkdnEnter
+        -- MkdnFollowLink = false,                                -- see MkdnEnter
+        -- MkdnDestroyLink = { "n", "<M-CR>" },
+        -- MkdnTagSpan = { "v", "<M-CR>" },
+        -- MkdnMoveSource = { "n", "<F2>" },
+        -- MkdnYankAnchorLink = { "n", "yaa" },
+        -- MkdnYankFileAnchorLink = { "n", "yfa" },
+        -- MkdnIncreaseHeading = { "n", "+" },
+        -- MkdnDecreaseHeading = { "n", "-" },
+        -- MkdnToggleToDo = { { "n", "v" }, "<C-Space>" },
+        -- MkdnNewListItem = false,
+        -- MkdnNewListItemBelowInsert = { "n", "o" },
+        -- MkdnNewListItemAboveInsert = { "n", "O" },
+        -- MkdnExtendList = false,
+        -- MkdnUpdateNumbering = { "n", "<leader>nn" },
+        -- MkdnTableNextCell = { "i", "<Tab>" },
+        -- MkdnTablePrevCell = { "i", "<S-Tab>" },
+        -- MkdnTableNextRow = false,
+        -- MkdnTablePrevRow = { "i", "<M-CR>" },
+        -- MkdnTableNewRowBelow = { "n", "<leader>ir" },
+        -- MkdnTableNewRowAbove = { "n", "<leader>iR" },
+        -- MkdnTableNewColAfter = { "n", "<leader>ic" },
+        -- MkdnTableNewColBefore = { "n", "<leader>iC" },
+        -- MkdnFoldSection = { "n", "<leader>f" },
+        -- MkdnUnfoldSection = { "n", "<leader>F" },
+    },
+})
