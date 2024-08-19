5a13ee59a89d310c18023341215db7075b22c01f
Trent Deal <trent.deal@gmail.com>
2024-08-19 16:53:31 -0400

    Switch to different nord scolorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,22 +2,12 @@ return {
   { "catppuccin/nvim", name = "catppuccin", enabled = false },
   { "folke/tokyonight.nvim", enabled = false },
   {
-    "shaunsingh/nord.nvim",
+    "gbprod/nord.nvim",
+    lazy = false,
+    priority = 1000,
     config = function()
-      -- require("nord").setup({
-      --   transparent = false,
-      --   borders = true,
-      -- })
-
-      -- vim.cmd.colorscheme("nord")
-      vim.g.nord_contrast = false
-      vim.g.nord_borders = true
-      vim.g.nord_bold = true
-      vim.g.nord_disable_background = true
-      vim.g.nord_cursorline_transparent = false
-      vim.g.nord_enable_sidebar_background = false
-      vim.g.nord_italic = true
-      vim.g.nord_uniform_diff_background = true
+      require("nord").setup({})
+      vim.cmd.colorscheme("nord")
     end,
   },
 }
