4d695fb7609468b05c0272a2b00c208bf5af4391
Trent Deal <trent.deal@gmail.com>
2024-12-19 15:18:00 -0500

    Update lazyvim to latest version

---
+++ .config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,26 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    keymap = {
+      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+      ["<C-e>"] = { "hide" },
+      ["<C-y>"] = { "select_and_accept" },
+
+      ["<UP>"] = { "select_prev", "fallback" },
+      ["<DOWN>"] = { "select_next", "fallback" },
+      ["<C-p>"] = { "select_prev", "fallback" },
+      ["<C-n>"] = { "select_next", "fallback" },
+      ["<Tab>"] = { "snippet_forward", "select_next", "fallback" },
+      ["<S-Tab>"] = { "snippet_backward", "select_prev", "fallback" },
+
+      ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+    },
+
+    completion = {
+      list = {
+        selection = "auto_insert",
+      },
+    },
+  },
+}
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,26 +1,27 @@
-return {
-  "nvim-telescope/telescope.nvim",
-  opts = function(_, opts)
-    local actions = require("telescope.actions")
-
-    opts.defaults = vim.tbl_extend("force", opts.defaults, {
-      path_display = { "truncate" },
-      sorting_strategy = "ascending",
-      mappings = {
-        i = {
-          ["<C-n>"] = actions.cycle_history_next,
-          ["<C-p>"] = actions.cycle_history_prev,
-          ["<C-j>"] = actions.move_selection_next,
-          ["<C-k>"] = actions.move_selection_previous,
-        },
-      },
-      layout_config = {
-        horizontal = { prompt_position = "top", preview_width = 0.55 },
-        vertical = { mirror = false },
-        width = 0.87,
-        height = 0.80,
-        preview_cutoff = 120,
-      },
-    })
-  end,
-}
+return {}
+-- return {
+--   "nvim-telescope/telescope.nvim",
+--   opts = function(_, opts)
+--     local actions = require("telescope.actions")
+--
+--     opts.defaults = vim.tbl_extend("force", opts.defaults, {
+--       path_display = { "truncate" },
+--       sorting_strategy = "ascending",
+--       mappings = {
+--         i = {
+--           ["<C-n>"] = actions.cycle_history_next,
+--           ["<C-p>"] = actions.cycle_history_prev,
+--           ["<C-j>"] = actions.move_selection_next,
+--           ["<C-k>"] = actions.move_selection_previous,
+--         },
+--       },
+--       layout_config = {
+--         horizontal = { prompt_position = "top", preview_width = 0.55 },
+--         vertical = { mirror = false },
+--         width = 0.87,
+--         height = 0.80,
+--         preview_cutoff = 120,
+--       },
+--     })
+--   end,
+-- }
