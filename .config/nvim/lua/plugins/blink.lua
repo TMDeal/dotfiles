0b806e7fa4e7ef7b77484243db241cec10b0482c
Trent Deal <trent.deal@gmail.com>
2025-02-01 15:06:32 -0500

    Update nvim configs

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -19,7 +19,10 @@ return {
 
     completion = {
       list = {
-        selection = "auto_insert",
+        selection = {
+          preselect = true,
+          auto_insert = true,
+        },
       },
     },
   },
